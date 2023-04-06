556b37a380b9089cede2757bb04e5fa3a59178d9
luiza-paloschi <luizapaloschi@gmail.com>
2023-04-06 20:39:56 -0300

    typeScript configuration and server setup

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import "express-async-errors";
+import express, { json } from "express";
+import cors from "cors";
+
+const app = express();
+app.use(json());
+app.use(cors());
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running in port: ${port}`));
